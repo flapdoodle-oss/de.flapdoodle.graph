6746ff8b3173d8affc5a322d94d194f0496622c5
Michael Mosmann <michael@mosmann.de>
2021-12-11 08:41:21 +0100

    subgraph isolation not done yet

--- src/main/java/de/flapdoodle/graph/GraphAsDot.java
+++ src/main/java/de/flapdoodle/graph/GraphAsDot.java
@@ -16,6 +16,12 @@
  */
 package de.flapdoodle.graph;
 
+import org.immutables.builder.Builder.Parameter;
+import org.immutables.value.Value;
+import org.immutables.value.Value.Auxiliary;
+import org.immutables.value.Value.Default;
+import org.jgrapht.Graph;
+
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -26,20 +32,20 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.immutables.builder.Builder.Parameter;
-import org.immutables.value.Value;
-import org.immutables.value.Value.Auxiliary;
-import org.immutables.value.Value.Default;
-import org.jgrapht.Graph;
-
 @Value.Immutable
 public abstract class GraphAsDot<T> {
 
-	@Parameter public abstract Function<T, String> nodeAsString();
-	
+	@Parameter
+	public abstract Function<T, String> nodeAsId();
+
+	@Default
+	public Function<T, String> nodeAsLabel() {
+		return nodeAsId();
+	}
+
 	@Default
 	public BiFunction<T, T, Map<String, String>> edgeAttributes() {
-		return (a,b) -> Collections.emptyMap();
+		return (a, b) -> Collections.emptyMap();
 	}
 
 	@Default
@@ -56,16 +62,16 @@ public abstract class GraphAsDot<T> {
 	public String label() {
 		return "graph";
 	}
-	
+
 	@Auxiliary
 	public <E> String asDot(Graph<T, E> graph) {
-		StringBuilder sb=new StringBuilder();
+		StringBuilder sb = new StringBuilder();
 
 		sb.append("digraph \"").append(label()).append("\" {\n")
 			.append("	rankdir=LR;\n")
 			.append("\n");
 
-		AtomicInteger clusterCounter=new AtomicInteger();
+		AtomicInteger clusterCounter = new AtomicInteger();
 
 		render(1, graph, sb, clusterCounter);
 
@@ -73,79 +79,96 @@ public abstract class GraphAsDot<T> {
 		return sb.toString();
 	}
 
+	// TODO put this stuff into context
 	private <E> void render(int level, Graph<T, E> graph, StringBuilder sb, AtomicInteger clusterCounter) {
 		renderNodes(level, graph, sb, clusterCounter);
 		sb.append("\n");
 
-		List<SubGraph<T>> subGraphs = graph.vertexSet().stream()
-			.flatMap(v -> subGraph().apply(v).map(sub -> Stream.of(sub)).orElse(Stream.empty()))
+		List<Vertex2SubGraph<T>> subGraphs = graph.vertexSet().stream()
+			.flatMap(v -> subGraph().apply(v)
+				.map(Stream::of)
+				.orElse(Stream.empty())
+				.map(sub -> new Vertex2SubGraph<>(v, sub)))
 			.collect(Collectors.toList());
 
-		Map<T, T> outerVertexToInnerVertexMap = subGraphs.stream()
-			.flatMap(sub -> sub.connections().entrySet().stream())
-			.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+		List<Vertext2VertexInSubGraph<T>> outerVertexToInnerVertexList = subGraphs.stream()
+			.flatMap(sub -> sub.subGraph.connections().entrySet().stream()
+				.map(entry -> new Vertext2VertexInSubGraph<>(
+					entry.getKey(), new VertexInSubGraph<>(sub.vertext, entry.getValue()))
+				)
+			)
+			.collect(Collectors.toList());
+
+		Map<T, VertexInSubGraph<T>> outerVertexToInnerVertexMap = outerVertexToInnerVertexList.stream()
+			.collect(Collectors.toMap(v -> v.vertex, v -> v.vertexInSubGraph));
 
 		renderEdges(level, graph, outerVertexToInnerVertexMap, sb);
 	}
 
 	private <E> void renderNodes(int level, Graph<T, E> graph, StringBuilder sb, AtomicInteger clusterCounter) {
 		graph.vertexSet().forEach(v -> {
+			Map<String, String> nodeAttributes = nodeAttributes().apply(v);
+
 			Optional<SubGraph<T>> subGraph = subGraph().apply(v);
 			if (subGraph.isPresent()) {
-				    sb.append(indent(level)).append("subgraph cluster_"+ clusterCounter.getAndIncrement()+" {\n");
-						sb.append(indent(level+1)).append("label = ").append(quote(nodeAsString().apply(v))).append(";\n");
-						render(level + 1, subGraph.get().graph(), sb, clusterCounter);
-						sb.append(indent(level)).append("}\n");
+				sb.append(indent(level)).append("subgraph cluster_" + clusterCounter.getAndIncrement() + " {\n");
+				if (!nodeAttributes.isEmpty()) {
+					sb.append(indent(level + 1)).append("node ").append(asNodeAttributes(nodeAttributes)).append(";\n");
+				}
+
+				render(level + 1, subGraph.get().graph(), sb, clusterCounter);
+				sb.append(indent(level)).append("}\n");
 			} else {
-				sb.append(indent(level)).append(quote(nodeAsString().apply(v))).append(asNodeAttributes(nodeAttributes().apply(v))).append(";\n");
+				sb.append(indent(level)).append(quote(nodeAsId().apply(v))).append(asNodeAttributes(nodeAttributes)).append(";\n");
 			}
 		});
 	}
 
-	private <E> void renderEdges(int level, Graph<T, E> graph, Map<T, T> outerVertexToInnerVertexMap, StringBuilder sb) {
+	private <E> void renderEdges(int level, Graph<T, E> graph, Map<T, VertexInSubGraph<T>> outerVertexToInnerVertexMap, StringBuilder sb) {
 		graph.edgeSet().forEach((edge) -> {
 			T a = graph.getEdgeSource(edge);
 			T b = graph.getEdgeTarget(edge);
 
-			T innerA = outerVertexToInnerVertexMap.get(a);
-			T innerB = outerVertexToInnerVertexMap.get(b);
+			VertexInSubGraph<T> innerA = outerVertexToInnerVertexMap.get(a);
+			VertexInSubGraph<T> innerB = outerVertexToInnerVertexMap.get(b);
 
 			if (!subGraph().apply(a).isPresent() && !subGraph().apply(b).isPresent()) {
-				sb.append(indent(level));
 				renderConnection(level, a, b, sb);
 			} else {
-				if (innerA!=null) {
-					sb.append(indent(level));
-					renderConnection(level, a, innerA, sb);
+				if (innerA != null) {
+					renderConnection(level, nodeAsId().apply(a), nodeAsId().apply(innerA.vertex), edgeAttributes().apply(a, innerA.vertex),  sb);
 				}
-				if (innerB!=null) {
-					sb.append(indent(level));
-					renderConnection(level, innerB, b, sb);
+				if (innerB != null) {
+					renderConnection(level, nodeAsId().apply(innerB.vertex), nodeAsId().apply(b), edgeAttributes().apply(innerB.vertex, b), sb);
 				}
 			}
 		});
 	}
+
 	private void renderConnection(int level, T a, T b, StringBuilder sb) {
-		sb.append(quote(nodeAsString().apply(a)))
-			.append(" -> ")
-			.append(quote(nodeAsString().apply(b)))
-			.append(asNodeAttributes(edgeAttributes().apply(a, b))).append(";\n");
+		renderConnection(level, nodeAsId().apply(a), nodeAsId().apply(b), edgeAttributes().apply(a, b), sb);
 	}
 
-	protected String indent(int level) {
+	private static void renderConnection(int level, String a, String b, Map<String, String> edgeAttributes, StringBuilder sb) {
+		sb.append(indent(level));
+		sb.append(quote(a))
+			.append(" -> ")
+			.append(quote(b))
+			.append(asNodeAttributes(edgeAttributes)).append(";\n");
+	}
+
+	private static String indent(int level) {
 		return String.join("", Collections.nCopies(level, "\t"));
 	}
 
-	@Auxiliary
-	public String asNodeAttributes(Map<String, String> map) {
-		return map.isEmpty() 
-				? "" 
-				: "[ "+map.entrySet().stream().map(e -> e.getKey()+"="+quote(e.getValue())).collect(Collectors.joining(", ")) +" ]";
+	private static String asNodeAttributes(Map<String, String> map) {
+		return map.isEmpty()
+			? ""
+			: "[ " + map.entrySet().stream().map(e -> e.getKey() + "=" + quote(e.getValue())).collect(Collectors.joining(", ")) + " ]";
 	}
 
-	@Auxiliary
-	public String quote(String src) {
-		return "\""+src+"\"";
+	private static String quote(String src) {
+		return "\"" + src + "\"";
 	}
 
 	@Value.Immutable
@@ -155,30 +178,42 @@ public abstract class GraphAsDot<T> {
 
 		Map<T, T> connections();
 
-		static <T> ImmutableSubGraph.Builder<T> of(Graph<T,?> graph) {
+		static <T> ImmutableSubGraph.Builder<T> of(Graph<T, ?> graph) {
 			return ImmutableSubGraph.builder(graph);
 		}
 	}
 
-	private static class Pair<A,B> {
-		private final A a;
-		private final B b;
+	private static final class Vertex2SubGraph<T> {
+		private final T vertext;
+		private final SubGraph<T> subGraph;
 
-		public Pair(A a, B b) {
-			this.a = a;
-			this.b = b;
-		}
-
-		public A a() {
-			return a;
-		}
-
-		public B b() {
-			return b;
+		public Vertex2SubGraph(T vertext, SubGraph<T> subGraph) {
+			this.vertext = vertext;
+			this.subGraph = subGraph;
 		}
 	}
 
-	public static <T> ImmutableGraphAsDot.Builder<T> builder(Function<T, String> nodeAsString) {
-		return ImmutableGraphAsDot.builder(nodeAsString);
+	private static final class Vertext2VertexInSubGraph<T> {
+		private final T vertex;
+		private final VertexInSubGraph<T> vertexInSubGraph;
+
+		public Vertext2VertexInSubGraph(T vertex, VertexInSubGraph<T> vertexInSubGraph) {
+			this.vertex = vertex;
+			this.vertexInSubGraph = vertexInSubGraph;
+		}
+	}
+
+	private static final class VertexInSubGraph<T> {
+		private final T parent;
+		private final T vertex;
+
+		public VertexInSubGraph(T parent, T vertex) {
+			this.parent = parent;
+			this.vertex = vertex;
+		}
+	}
+
+	public static <T> ImmutableGraphAsDot.Builder<T> builder(Function<T, String> nodeAsId) {
+		return ImmutableGraphAsDot.builder(nodeAsId);
 	}
 }
--- src/test/java/de/flapdoodle/graph/GraphAsDotTest.java
+++ src/test/java/de/flapdoodle/graph/GraphAsDotTest.java
@@ -27,7 +27,11 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.IdentityHashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -54,38 +58,71 @@ class GraphAsDotTest {
 
 	@Test
 	void subGraphAsDot() {
-		DefaultDirectedGraph<Vertex, DefaultEdge> sub = Graphs.with(Graphs.graphBuilder(Graphs.directedGraph(Vertex.class, DefaultEdge.class)))
+		Vertex.Named oneX = Vertex.Named.of("x");
+		Vertex.Named oneZ = Vertex.Named.of("z");
+
+		DefaultDirectedGraph<Vertex, DefaultEdge> oneSub = Graphs.with(Graphs.graphBuilder(Graphs.directedGraph(Vertex.class, DefaultEdge.class)))
 			.build(builder -> {
-				Vertex.Named x = Vertex.Named.of("x");
+				Vertex.Named x = oneX;
 				Vertex.Named y = Vertex.Named.of("y");
-				Vertex.Named z = Vertex.Named.of("z");
+				Vertex.Named z = oneZ;
 				builder.addVertices(x, y, z);
 				builder.addEdge(x, y);
 				builder.addEdge(y, z);
 			});
 
-		ImmutableWithGraph subGraph = Vertex.WithGraph.of("B", sub)
-			.in(Vertex.Named.of("B.in"))
-			.out(Vertex.Named.of("B.out"))
-			.putConnections(Vertex.Named.of("B.in"), Vertex.Named.of("x"))
-			.putConnections(Vertex.Named.of("B.out"), Vertex.Named.of("z"))
+		Vertex.Named otherX = Vertex.Named.of("x");
+		Vertex.Named otherZ = Vertex.Named.of("z");
+
+		DefaultDirectedGraph<Vertex, DefaultEdge> otherSub = Graphs.with(Graphs.graphBuilder(Graphs.directedGraph(Vertex.class, DefaultEdge.class)))
+			.build(builder -> {
+				Vertex.Named x = otherX;
+				Vertex.Named y = Vertex.Named.of("y");
+				Vertex.Named z = otherZ;
+				builder.addVertices(x, y, z);
+				builder.addEdge(x, y);
+				builder.addEdge(y, z);
+			});
+
+		ImmutableWithGraph oneSubGraph = Vertex.WithGraph.of("One", oneSub)
+			.in(Vertex.Named.of("One.in"))
+			.out(Vertex.Named.of("One.out"))
+			.putConnections(Vertex.Named.of("One.in"), oneX)
+			.putConnections(Vertex.Named.of("One.out"), oneZ)
+			.build();
+
+		ImmutableWithGraph otherSubGraph = Vertex.WithGraph.of("Other", otherSub)
+			.in(Vertex.Named.of("Other.in"))
+			.out(Vertex.Named.of("Other.out"))
+			.putConnections(Vertex.Named.of("Other.in"), otherX)
+			.putConnections(Vertex.Named.of("Other.out"), otherZ)
 			.build();
 
 		DefaultDirectedGraph<Vertex, DefaultEdge> graph = Graphs.with(Graphs.graphBuilder(Graphs.directedGraph(Vertex.class, DefaultEdge.class)))
 			.build(builder -> {
 				Vertex.Named a = Vertex.Named.of("A");
-				Vertex.WithGraph b = subGraph;
+				Vertex.WithGraph one = oneSubGraph;
+				Vertex.WithGraph other = otherSubGraph;
 				Vertex.Named c = Vertex.Named.of("C");
 
-				builder.addVertices(a, b.in(), b, b.out(), c);
-				builder.addEdge(a, b.in());
-				builder.addEdge(b.in(), b);
-				builder.addEdge(b, b.out());
-				builder.addEdge(b.out(), c);
-				
+				builder.addVertices(a, c);
+				builder.addVertices(one.in(), one, one.out());
+				builder.addVertices(other.in(), other, other.out());
+
+				builder.addEdge(a, one.in());
+				builder.addEdge(one.in(), one);
+				builder.addEdge(one, one.out());
+				builder.addEdge(one.out(), c);
+
+				builder.addEdge(a, other.in());
+				builder.addEdge(other.in(), other);
+				builder.addEdge(other, other.out());
+				builder.addEdge(other.out(), c);
+
+				builder.addEdge(a, c);
 			});
 
-		String dotFile = GraphAsDot.<Vertex>builder(Vertex::name)
+		String dotFile = GraphAsDot.<Vertex>builder(nameAndCounter())
 			.subGraph(v -> {
 				if (v instanceof Vertex.WithGraph) {
 					Vertex.WithGraph withGraph = (Vertex.WithGraph) v;
@@ -96,11 +133,32 @@ class GraphAsDotTest {
 				}
 				return Optional.empty();
 			})
+			.nodeAttributes(vertex -> mapOf("label", vertex.name()))
 			.build()
 			.asDot(graph);
 
 		assertThat(dotFile)
 			.isEqualTo(dotFile("subgraph.dot"));
+	}
+	
+	private static Map<String, String> mapOf(String k1, String v1) {
+		LinkedHashMap<String, String> ret = new LinkedHashMap<>();
+		ret.put(k1,v1);
+		return ret;
+	}
+
+	private Function<Vertex, String> nameAndCounter() {
+		AtomicInteger counter=new AtomicInteger();
+		IdentityHashMap<Vertex, Integer> identityHashCodeCounter=new IdentityHashMap<>();
+
+		return vertex -> {
+			Integer id = identityHashCodeCounter.get(vertex);
+			if (id==null) {
+				id=counter.getAndIncrement();
+				identityHashCodeCounter.put(vertex,id);
+			}
+			return vertex.name()+"#"+id;
+		};
 	}
 
 	private static String dotFile(String name) {
